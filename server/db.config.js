d77f62b301367efb66af0667f4b90c854bae7e78
Sanatan Shrivastava <sanatan.cseiiitk@gmail.com>
2021-05-20 18:56:06 +0530

    initial push

---
+++ server/db.config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyB2d0JLSXdrqGJWfKvSTVL-Va2bvy2xhEo",
+  authDomain: "dysgraphia-ff49f.firebaseapp.com",
+  projectId: "dysgraphia-ff49f",
+  storageBucket: "dysgraphia-ff49f.appspot.com",
+  messagingSenderId: "979606011290",
+  appId: "1:979606011290:web:841ea0b94cb022adc30e23",
+  measurementId: "G-5X3CMKJZWV",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
